a74948c524d3f45696a638b3526c92d7aa2bb71e
aks7665 <aks199628@gmail.com>
2020-11-23 12:53:27 +0530

    Table

--- src/app/dashboard/report/report.component.ts
+++ src/app/dashboard/report/report.component.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
 import { AppState } from 'src/app/store';
 import { fetchAllProducts } from 'src/app/store/product/product.selectors';
-
+import * as moment from 'moment';
 import { fetchAllTransactions } from 'src/app/store/transaction/transaction.selectors';
 import { Transaction } from 'src/app/models/transaction.model';
 import { PdfHelperService } from 'src/app/shared/helper/pdf-helper.service';
@@ -40,11 +40,12 @@ export class ReportComponent implements OnInit {
     return this.transactions.filter((trans) => trans.productId === productId);
   }
 
-  downloadPDF(): void {
+  downloadReport(): void {
     const header = ['Item Name', 'Available Quantity'];
-    const body = this.products.map(trans => {
-      return [trans.title, trans.quantity];
+    const body = this.products.map((product) => {
+      return [product.title, product.quantity];
     });
+    // Pushing header to table top
     body.unshift(header);
 
     const docDefinition = {
@@ -54,13 +55,78 @@ export class ReportComponent implements OnInit {
           table: {
             headerRows: 1,
             widths: ['auto', 'auto'],
-            body
-          }
-        }
-      ]
+            body,
+          },
+        },
+      ],
     };
 
     this.pdfHelperService.downloadPdf(docDefinition);
   }
 
+  downloadDetailReport(): void {
+    const headers = ['Transaction Id', 'Date/Time', 'Type', 'Quantity'];
+    const content = [];
+    this.products.map((product) => {
+      // Generating title & quatity heading of item
+      content.push({
+        text: `Item Name: ${product.title}`,
+        style: 'subheader',
+        bold: true,
+      });
+      content.push({
+        text: `Available Quantity: ${product.quantity}`,
+        bold: true,
+        style: 'subheader',
+      });
+      const singleProductTrans = this.getTransationByProductId(product._id);
+      // Generating transaction tables itemwise
+      let finalTrans = [];
+      if (singleProductTrans && singleProductTrans.length > 0) {
+        // Generating rows
+        finalTrans = singleProductTrans.map((trans) => [
+          trans.transationId,
+          moment(trans.createdAt).format('y-MM-d HH:mm:ss'),
+          trans.transationType.toUpperCase(),
+          trans.quantity,
+        ]);
+      }
+      if (finalTrans.length > 0) {
+        // Pushing header to table top
+        finalTrans.unshift(headers);
+        content.push({
+          table: {
+            body: finalTrans,
+          },
+        });
+      }
+      content.push({ text: '-------------------------------------', style: 'subheader' });
+    });
+
+    const docDefinition = {
+      content,
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10]
+        },
+        subheader: {
+          fontSize: 16,
+          bold: true,
+          margin: [0, 10, 0, 5]
+        },
+        tableExample: {
+          margin: [0, 5, 0, 15]
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 13,
+          color: 'black'
+        }
+      },
+    };
+
+    this.pdfHelperService.downloadPdf(docDefinition);
+  }
 }
